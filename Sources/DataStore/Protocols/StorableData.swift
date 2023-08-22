a782a97c0295c2e36e307f3eeaaf89580b18ee5a
Leif <zmeriksen@icloud.com>
2023-08-21 20:26:52 -0600

    Update documentation

--- Sources/DataStore/Protocols/StorableData.swift
+++ Sources/DataStore/Protocols/StorableData.swift
@@ -1,7 +1,7 @@
 /**
  A protocol that represents storable data that can be adapted into `OnDeviceData`.
 
- The `StorableData` protocol requires conforming types to be adaptable, identifiable, and represent data that can be stored on a device.
+ The `StorableData` protocol requires conforming types to be adaptable and represent data that can be stored on a device.
  The generic type `To` represents the type that conforms to the `OnDeviceData` protocol and will be used to store the current data.
 
  Conforming types must ensure that the `To` generic type conforms to the `OnDeviceData` protocol and that its `StoredValue` type is the same as the current data type.
